19504bcb4554a6c22f35d2b17e8cd142340b7825
Pavel Ryabov <pavelarz14@yandex.ru>
2026-05-30 10:37:24 +0300

    new

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -131,3 +131,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000'
 ]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
